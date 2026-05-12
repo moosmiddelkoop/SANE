10713ed8b7cec9a1b1c9f142ef89d49caea19cff
Moos <moos@cwi.nl>
2026-05-12 14:34:49 +0200

    larger epoch range and formatting

--- data/preprocess_dataset_cnn_cifar10.py
+++ data/preprocess_dataset_cnn_cifar10.py
@@ -1,16 +1,14 @@
 # prepare data
+import logging
 from pathlib import Path
 
+import torch
+
+from SANE.datasets.dataset_preprocessing import prepare_multiple_datasets
+from SANE.datasets.dataset_sampling_preprocessed import PreprocessedSamplingDataset
 from SANE.git_re_basin.git_re_basin import (
     zoo_cnn_large_permutation_spec,
 )
-
-from SANE.datasets.dataset_preprocessing import prepare_multiple_datasets
-from SANE.datasets.dataset_properties import PropertyDataset
-from SANE.datasets.dataset_sampling_preprocessed import PreprocessedSamplingDataset
-
-import logging
-import torch
 
 logging.basicConfig(level=logging.INFO)
 
@@ -57,11 +55,9 @@ logging.basicConfig(level=logging.INFO)
 
 def prep_data():
     dataset_target_path = [
-        Path("/scratch-shared/mmiddelkoop/SANE/data/dataset_cnn_cifar10_ep21-25_std/"),
+        Path("/scratch-shared/mmiddelkoop/SANE/data/dataset_cnn_cifar10_ep05-50_std/"),
     ]
-    zoo_path = [  
-        Path("/scratch-shared/mmiddelkoop/SANE/data/tune_zoo_cifar10_uniform_large/").absolute()
-    ]
+    zoo_path = [Path("/scratch-shared/mmiddelkoop/SANE/data/tune_zoo_cifar10_uniform_large/").absolute()]
     zoo_path_and_permutation_spec_and_target_path = [
         (zoo_path[0], zoo_cnn_large_permutation_spec, dataset_target_path[0]),
     ]
@@ -84,11 +80,11 @@ def prep_data():
 
 def create_configurations(zoo_path_and_permutation_spec_and_target_path, filter_fn=None):
     # static parameters
-    epoch_list = [21, 22, 23, 24, 25]
+    epoch_list = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
     map_to_canonical = True
     standardize = True
     ds_split = [0.7, 0.15, 0.15]
-    max_samples = 10
+    max_samples = None
     weight_threshold = 100
     num_threads = 5
     shuffle_path = True
@@ -96,7 +92,7 @@ def create_configurations(zoo_path_and_permutation_spec_and_target_path, filter_
     supersample = 3
     precision = "32"
     ignore_bn = True
-    tokensize = 0  #discover tokensize
+    tokensize = 0  # discover tokensize
 
     # permutation spec
     permutation_number_train = 200
@@ -146,7 +142,7 @@ def create_configurations(zoo_path_and_permutation_spec_and_target_path, filter_
                     "filter_fn": filter_fn,
                 }
             )
-    
+
     return configurations
 
 
