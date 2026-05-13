1b01065e3968aa04be3a5a0973bea33f23422bf9
Moos <moos@cwi.nl>
2026-05-13 15:09:27 +0200

    v4

--- data/preprocess_cnn_cifar10.sh
+++ data/preprocess_cnn_cifar10.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=preprocess_cnn_cifar100
+#SBATCH --job-name=preprocess_cnn_cifar100_v4
 #SBATCH --partition=gpu_a100
 #SBATCH --gpus=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=18
 #SBATCH --time=12:00:00
-#SBATCH --output=logs/preprocess_cnn_%j.out
-#SBATCH --error=logs/preprocess_cnn_%j.err
+#SBATCH --output=logs/preprocess_cnn_v4%j.out
 #SBATCH --mail-type=END
 #SBATCH --mail-user=moos@cwi.nl
 
